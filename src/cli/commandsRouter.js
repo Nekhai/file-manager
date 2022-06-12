18c6c4f185512dbf9450dfa239d2a880e64b32e6
Nekhai <NekhaiYurii@gmail.com>
2022-06-12 23:07:54 +0300

    feat: add router commands

--- src/cli/commandsRouter.js
+++ src/cli/commandsRouter.js
@@ -1,8 +1,21 @@
-import { ls, up, cd, os } from "../utils/index.js";
+import {
+  ls,
+  up,
+  cd,
+  os,
+  cat,
+  add,
+  rn,
+  cp,
+  mv,
+  del,
+  hash,
+  compress,
+  decompress,
+} from "../utils/index.js";
 import { EOL } from "os";
 
 export const commandsRouter = async (command) => {
-  const arg = command.slice(3);
   try {
     switch (true) {
       case command === "ls":
@@ -12,9 +25,37 @@ export const commandsRouter = async (command) => {
         up();
         break;
       case command.startsWith("cd"):
+        cd(command.slice(3));
         break;
       case command.startsWith("os"):
-        os(arg);
+        os(command.slice(3));
+        break;
+      case command.startsWith("cat"):
+        cat(command.slice(4));
+        break;
+      case command.startsWith("add"):
+        add(command.slice(4));
+        break;
+      case command.startsWith("rn"):
+        rn(command.slice(3));
+        break;
+      case command.startsWith("rm"):
+        del(command.slice(3));
+        break;
+      case command.startsWith("cp"):
+        cp(command.slice(3));
+        break;
+      case command.startsWith("mv"):
+        mv(command.slice(3));
+        break;
+      case command.startsWith("hash"):
+        hash(command.slice(5));
+        break;
+      case command.startsWith("compress"):
+        compress(command.slice(9));
+        break;
+      case command.startsWith("decompress"):
+        decompress(command.slice(11));
         break;
       default:
         console.log("Invalid input");
